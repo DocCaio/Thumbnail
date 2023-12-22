b33da41817c3c5489124d387c5949cd3f197e232
Caio <108128119+DocCaio@users.noreply.github.com>
2023-12-22 13:56:20 -0300

    up

---
+++ app/main.js
@@ -0,0 +1,35 @@
+const imagemFlutuante = document.querySelector(".imageFlying");
+const texto = "Hallo I m Caio.";
+const elementoMaquinaEscrever = document.querySelector(".typewriter");
+
+function animarFlutuacao() {
+  let distancia = 20; // Distância vertical da flutuação em pixels
+  let duracao = 2000; // Duração da animação em milissegundos
+
+  imagemFlutuante.style.transition = `transform ${duracao}ms ease-in-out`;
+  imagemFlutuante.style.transform = `translateY(-${distancia}px)`;
+
+  setTimeout(() => {
+    imagemFlutuante.style.transition = `transform ${duracao}ms ease-in-out`;
+    imagemFlutuante.style.transform = `translateY(${distancia}px)`;
+
+    setTimeout(() => {
+      animarFlutuacao();
+    }, duracao);
+  }, duracao);
+}
+
+let indice = 0;
+
+function escreverTexto() {
+  if (indice < texto.length) {
+    elementoMaquinaEscrever.textContent += texto.charAt(indice);
+    indice++;
+    setTimeout(escreverTexto, 100); // Ajuste a velocidade da digitação aqui (em milissegundos)
+  }
+}
+
+// Iniciar a animação quando a página carregar
+window.addEventListener("load", animarFlutuacao);
+// Iniciar a animação quando a página carregar
+window.addEventListener("load", escreverTexto);
